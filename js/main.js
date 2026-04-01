05b358d85f55f1582663818407f00b5823c0b7a1
stol25jr <stol25jr@student.ju.se>
2026-04-01 14:25:38 +0200

    Changed various elements in html and css file. Tested js functionality.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const buttonElement = document.getElementById("button");
+
+buttonElement.addEventListener("click", clickFunction);
+{
+  localStorage.product = button;
+}
+
+function clickFunction() {
+  console.log("The button was clicked!");
+}
